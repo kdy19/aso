650f07e8eb78bd43f618e316a57375b858cf5734
kdy19 <qweasd0717@icloud.com>
2021-06-06 14:18:02 +0000

    http_sniff.py

--- http_sniff.py
+++ http_sniff.py
@@ -4,30 +4,37 @@ import sys
 import os
 
 def show_packet(packet) -> None:
+	
+	try:
+		if (packet[IP].dst == '192.168.0.5') and (packet[TCP].dport == 22):
+			string = '{}  {}:{} -> {}:{}  [SSH]'.format(time.strftime(
+				'%Y-%m-%d %H:%M:%S', time.localtime(time.time())),
+				packet[IP].src, packet[IP].sport,
+				packet[IP].dst, packet[IP].dport)
+			print(string)
 
-    f = open('C:\\Users\\kdy\\Desktop\\8000.log', 'a')
+		elif (packet[IP].dst == '192.168.0.5') and (packet[TCP].dport == 80):
+			string = '{}  {}:{} -> {}:{}  [HTTP] flags: {}'.format(time.strftime(
+				'%Y-%m-%d %H:%M:%S', time.localtime(time.time())),
+				packet[IP].src, packet[IP].sport,
+				packet[IP].dst, packet[IP].dport,
+				packet[TCP].flags)
+			print(string)
 
-    try:
-        if packet[TCP].payload:
-            if packet[IP].dport == 8000:
-                string = "{} [HTTP] src={} {}\n".format(time.strftime(
-                    '%Y-%m-%d %H:%M:%S', time.localtime(time.time())),
-                    packet[IP].src,
-                    "\n".join(packet.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n")))
+		elif (packet[IP].dst == '192.168.0.5') and (packet[TCP].dport == 443):
+			string = '{}  {}:{} -> {}:{}  [HTTPS] flags: {}'.format(time.strftime(
+				'%Y-%m-%d %H:%M:%S', time.localtime(time.time())),
+				packet[IP].src, packet[IP].sport,
+				packet[IP].dst, packet[IP].dport,
+				packet[TCP].flags)
+			print(string)
 
-                print("\n".join(packet.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n")))
+		else:
+			pass
 
-                f.write(string)
-
-    except Exception as e:
-        print(sys.exc_info()[0], e)
-        f.close()
-
-    try:
-        f.close()
-    except Exception as e:
-        print(sys.exc_info()[0], e)
+	except Exception as e:
+		print(sys.exc_info()[0], e)
 
 
 if __name__ == "__main__":
-    sniff(filter='tcp', prn=show_packet, store=0, count=0)
+    sniff(filter='tcp', prn=show_packet, store=0, count=10)
