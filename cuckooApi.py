c3bb6c4222be4fc7e31807ca22c67b2b4bd827b2
kdy19 <qweasd0717@icloud.com>
2021-08-30 10:07:08 -0700

    cuckoo

--- cuckooApi.py
+++ cuckooApi.py
@@ -1,86 +1,109 @@
-import requests
 import hashlib
-import sqlite3
-import py7zr
-import time
+import json
 import os
+import time
 
-cuckoo_API = "Bearer <cuckoo API>"
-
-url_send_file = "http://192.168.106.128:8090/tasks/create/file"
-header = {"Authorization" : cuckoo_API}
-dir_path = "./"
+import py7zr
+import requests
 
 
-def status_print(file_name):
-    print(file_name, end=' ', flush=True)
-    for t in range(1, 10+1):
-        print("{}% ".format(t*10), end='', flush=True)
-        time.sleep(30)
+base_url = 'http://192.168.106.133:8090/'
+create_url = f'{base_url}tasks/create/file'
+download_url = f'{base_url}tasks/report'
+delete_url = f'{base_url}tasks/delete'
+
+directory_path = 'E:\\sample\\cfromAtoFAsterisk'
+download_path = 'E:\\t'
+
+header = {'Authorization': 'Bearer 3C-Y5VP4D-E-pGb1vM5BJw'}
+
+
+def status_sleep(status_id):
+    print(status_id, end=' ', flush=True)
+    for t in range(1, 10 + 1):
+        print("{}% ".format(t * 10), end='', flush=True)
+        time.sleep(27)
     print()
 
 
 def file_extract():
-    file_list = os.listdir(dir_path)
+    password = 'infected'
+
+    file_list = os.listdir(directory_path)
 
     for file in file_list:
         try:
-            with py7zr.SevenZipFile(file, mode='r', password="<password>") as z:
-                z.extractall()
-        except:
+            with py7zr.SevenZipFile(file, mode='r', password=password) as z:
+                z.extractall(path=f'{directory_path}')
+            os.remove(file)
+        except Exception as e:
+            print(e)
+
+
+def report_create():
+    file_list = os.listdir(directory_path)
+    cnt = 0
+    status_id = []
+    for fi in file_list:
+        if ('main.py' in fi) or ('.7z' in fi):
             continue
 
+        if '.exe' in fi:
+            with open(f'{directory_path}\\{fi}', 'rb') as f:
+                data = f.read()
 
-def file_transfer():
-    file_lists = os.listdir(dir_path)
+                res = requests.post(url=create_url, headers=header,
+                                    files={'file': (str(hashlib.sha256(data).hexdigest()) + '.exe', data)})
 
-    for file in file_lists:
-        status = "{} ".format(file)
+                task_id = res.json()['task_id']
+        else:
+            with open(f'{directory_path}\\{fi}', 'rb') as f:
+                data = f.read()
+
+                res = requests.post(url=create_url, headers=header,
+                                    files={'file': (str(hashlib.sha256(data).hexdigest()), data)})
+
+                task_id = res.json()['task_id']
+        cnt += 1
+        status_id.append(task_id)
+
+        if (cnt % 2) == 0:
+            status_sleep(status_id)
+            report_download(status_id)
+            cnt = 0
+            status_id = []
+
+
+def report_download(status_id):
+
+    for idx in status_id:
+        send_url = f'{download_url}/{idx}'
+
+        res = requests.get(url=send_url, headers=header)
+
         try:
-            extension = file.split('.')[1]
+            file_name = res.json()['target']['file']['sha256']
+        except Exception as e:
+            continue
 
-            if extension == "exe":
-                f = open(file, "rb")
-                f_data = f.read()
-
-                res = requests.post(url_send_file, headers=header, files={"file":
-                                (str(hashlib.sha256(f_data).hexdigest()) + '.' +
-                                 extension, f_data)})
-
-                f.close()
-
-                print(res.json()["task_id"], end=' ')
-                status_print(file)
-
-        except:
-            f = open(file, "rb")
-            f_data = f.read()
-
-            res = requests.post(url_send_file, headers=header, files={"file":
-                            (str(hashlib.sha256(f_data).hexdigest()), f_data)})
-
-            f.close()
-
-            print(res.json()["task_id"], end=' ')
-            status_print(file)
-
-
-def report_download():
-
-    for idx in range(1, 471+1):
-        url = f'http://192.168.106.128:8090/tasks/report/{i}'
-
-        res = requests.get(url, headers=header)
-        
-        file_name = res.json()['target']['file']['sha256']
-
-        with open(f'{file_name}.json', 'w') as f:
+        with open(f'{download_path}\\{file_name}.json', 'w') as f:
             json.dump(res.json(), f, indent=4)
 
-        print(f'[+] {file_name}')
+        report_delete(idx)
 
 
-if __name__=="__main__":
+def report_delete(task_id):
+
+    send_url = f'{delete_url}/{task_id}'
+
+    res = requests.get(url=send_url, headers=header)
+
+    try:
+        print(res.json()['status'])
+    except Exception as e:
+        pass
+
+
+if __name__ == '__main__':
     file_extract()
-    file_transfer()
-
+    report_create()
